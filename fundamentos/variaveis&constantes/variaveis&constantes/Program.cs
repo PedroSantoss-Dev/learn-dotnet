73d50a09c8e49b7643ba57381724c9ed7608018d
Pedro Santos <90013568+PedroSantoss-Dev@users.noreply.github.com>
2024-01-12 13:10:09 -0300

    feat: add constants and variables

---
+++ fundamentos/variaveis&constantes/variaveis&constantes/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace variaveis_constantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variáveis podem serem alteradas enquanto as constantes não podem sofrer alterações
+            // exemplo de variável
+            string nome;
+            nome = "Pedro";
+            int idade;
+            idade = 25;
+            bool feliz = true;
+            Console.WriteLine($"Nome:{nome} \n Idade: {idade} \n Feliz: {feliz}  ");
+
+            // exemplo de constante 
+            const int idade2 = 28;
+            Console.WriteLine(idade2);
+            Console.ReadKey();
+        }
+    }
+}
