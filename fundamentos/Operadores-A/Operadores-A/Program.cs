731f14075d27bc9759ddbb519c55406206c9eb97
Pedro Santos <90013568+PedroSantoss-Dev@users.noreply.github.com>
2024-01-12 20:17:21 -0300

    feat: add arithmetic operators

---
+++ fundamentos/Operadores-A/Operadores-A/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Operadores_A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 6;
+
+            //Soma
+            Console.WriteLine(num1 + num2);
+            //Subtração
+            Console.WriteLine(num1 - num2);
+            // Multiplicação
+            Console.WriteLine(num1 * num2);
+            // Divisão
+            Console.WriteLine(num1 / num2);
+            // Resto
+            Console.WriteLine( num1 % 2);
+            Console.ReadKey();
+        }
+    }
+}
