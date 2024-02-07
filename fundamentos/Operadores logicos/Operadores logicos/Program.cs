1d0a8a1e6c43448a33c202a84a6dda1a79506b7d
Pedro Santos <90013568+PedroSantoss-Dev@users.noreply.github.com>
2024-02-07 17:55:20 -0300

    feat: add operador logico

---
+++ fundamentos/Operadores logicos/Operadores logicos/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Operadores_logicos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool teste = true;
+            int A = 6; 
+            
+
+            if(A> 3 ||  teste)
+            {
+                Console.WriteLine("Passou");
+            }
+            else
+            {
+                Console.WriteLine("Falhou");
+
+            }
+
+            if (A > 3 && teste)
+            {
+                Console.WriteLine("Passou");
+            }
+            else
+            {
+                Console.WriteLine("Falhou");
+
+            }
+
+            if (!teste)
+            {
+                Console.WriteLine("Passou");
+            }
+            else
+            {
+                Console.WriteLine("Falhou");
+
+            }
+            Console.ReadKey();    
+        }   
+    }
+}
